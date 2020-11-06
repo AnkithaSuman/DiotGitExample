64625a391a1c4bf8e53bca1f2c6b3bfde2d39119
Ankitha Suman <19ankithasuman@gmail.com>
2020-11-06 10:58:06 +0530

    Multiple files are Commited

---
+++ thirdExample.java
@@ -0,0 +1,12 @@
+public class thirdExample {
+    public static void printOddNumbers(int n){
+        System.out.println("Odd number upto " + n);
+        for (int i = 0; i < n; i++) {
+            if(i%2!=0){
+                System.out.println(i);
+
+            }
+            
+        }
+}
+}
